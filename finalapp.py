ff57ceca66481ac408cc7ab3d71beab0866efc9e
Hrithik Shukla <122604143+hrithik133@users.noreply.github.com>
2024-03-22 11:27:56 -0400

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -9,6 +9,7 @@ import numpy as np
 #Refer: https://www.nyc.gov/site/nypd/stats/reports-analysis/subway-fare-evasion.page
 
 app = Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.H1("Subway Fare Evasion Dashboard"),
